62b025cad5c697f0382188a4d1c13800035cbcc5
jonathan <jonathan@kyuss.org>
2013-09-01 20:58:45 -0700

    move LocaleAwareMustacheFactory into cobbzilla-utils.

--- src/main/java/org/cobbzilla/mail/TemplatedMailSender.java
+++ src/main/java/org/cobbzilla/mail/TemplatedMailSender.java
@@ -1,15 +1,12 @@
 package org.cobbzilla.mail;
 
-import com.github.mustachejava.MustacheFactory;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cobbzilla.util.mustache.LocaleAwareMustacheFactory;
 import org.cobbzilla.util.mustache.MustacheResourceNotFoundException;
-import org.slf4j.Logger;
 
 import java.io.File;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +32,7 @@ public class TemplatedMailSender {
     private File fileRoot;
 
     public void deliverMessage (TemplatedMail mail) throws Exception {
+
         Map<String, Object> scope = mail.getParameters();
         if (scope == null) {
             log.warn("No parameters found to put in scope");
@@ -42,16 +40,15 @@ public class TemplatedMailSender {
         }
 
         final String templateName = mail.getTemplateName();
-        final MustacheFactory mustacheFactory = LocaleAwareMustacheFactory.getFactory(fileRoot, mail.getLocale());
+        final LocaleAwareMustacheFactory mustache = LocaleAwareMustacheFactory.getFactory(fileRoot, mail.getLocale());
 
-        final String fromName = render(templateName + FROMNAME_SUFFIX, scope, mustacheFactory);
-        final String fromEmail = render(templateName + FROMEMAIL_SUFFIX, scope, mustacheFactory);
+        final String fromName = mustache.render(templateName + FROMNAME_SUFFIX, scope);
+        final String fromEmail = mustache.render(templateName + FROMEMAIL_SUFFIX, scope);
         if (fromEmail == null) {
-            throw new IllegalArgumentException("No fromEmail template could be rendered for template: "+templateName);
+            throw new IllegalArgumentException("No fromEmail template could be mustache.rendered for template: "+templateName);
         }
-        final String cc = render(templateName + CC_SUFFIX, scope, mustacheFactory);
-        final String bcc = render(templateName + BCC_SUFFIX, scope, mustacheFactory);
-
+        final String cc = mustache.render(templateName + CC_SUFFIX, scope);
+        final String bcc = mustache.render(templateName + BCC_SUFFIX, scope);
 
         // we do not put "cc" and "bcc" into scope, as they should not be needed in the subject or textBody
         if (fromName != null) scope.put(SCOPE_FROM_NAME, fromName);
@@ -60,18 +57,18 @@ public class TemplatedMailSender {
         if (mail.getToName() != null) scope.put(SCOPE_TO_NAME, mail.getToName());
         scope.put(SCOPE_TO_EMAIL, mail.getToEmail());
 
-        final String subject = render(templateName + SUBJECT_SUFFIX, scope, mustacheFactory);
+        final String subject = mustache.render(templateName + SUBJECT_SUFFIX, scope);
         if (subject == null) {
-            throw new IllegalArgumentException("No subject template could be rendered for template: "+templateName+" (locale="+mail.getLocale()+")");
+            throw new IllegalArgumentException("No subject template could be mustache.rendered for template: "+templateName+" (locale="+mail.getLocale()+")");
         }
-        final String textBody = render(templateName+TEXT_SUFFIX, scope, mustacheFactory);
+        final String textBody = mustache.render(templateName+TEXT_SUFFIX, scope);
         if (textBody == null) {
-            throw new IllegalArgumentException("No text body template could be rendered for template: "+templateName+" (locale="+mail.getLocale()+")");
+            throw new IllegalArgumentException("No text body template could be mustache.rendered for template: "+templateName+" (locale="+mail.getLocale()+")");
         }
 
         String htmlBody = null;
         try {
-            htmlBody = render(templateName+HTML_SUFFIX, scope, mustacheFactory);
+            htmlBody = mustache.render(templateName+HTML_SUFFIX, scope);
         } catch (UncheckedExecutionException e) {
             if (e.getCause() instanceof MustacheResourceNotFoundException) {
                 log.warn("No HTML email template found for resource: "+templateName);
@@ -107,22 +104,6 @@ public class TemplatedMailSender {
         }
         data = data.substring(0, lastCurly+1);
         return data;
-    }
-
-    protected String render(String templateName, Map<String, Object> scope, MustacheFactory mustacheFactory) {
-        StringWriter writer = new StringWriter();
-        try {
-            mustacheFactory.compile(templateName).execute(writer, scope);
-        } catch (UncheckedExecutionException e) {
-            if (e.getCause() instanceof MustacheResourceNotFoundException) {
-                return null;
-            } else {
-                throw e;
-            }
-        } catch (MustacheResourceNotFoundException e) {
-            return null;
-        }
-        return writer.getBuffer().toString().trim();
     }
 
 }

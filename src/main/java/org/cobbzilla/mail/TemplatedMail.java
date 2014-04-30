60e8453387123627c313e87f749827892ac632c3
jonathan <jonathan@kyuss.org>
2014-04-30 03:09:20 -0700

    add constructor helpers

--- src/main/java/org/cobbzilla/mail/TemplatedMail.java
+++ src/main/java/org/cobbzilla/mail/TemplatedMail.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,10 +19,25 @@ public class TemplatedMail {
     private static final Logger LOG = LoggerFactory.getLogger(TemplatedMail.class);
 
     @NotNull @JsonProperty @Getter @Setter private String templateName;
+    public TemplatedMail withTemplateName (String templateName) { this.templateName = templateName; return this; }
+
     @JsonProperty @Getter @Setter private String locale;
+    public TemplatedMail withLocale (String locale) { this.locale = locale; return this; }
+
     @JsonProperty @Getter @Setter private String toName;
+    public TemplatedMail withToName (String toName) { this.toName = toName; return this; }
+
     @NotNull @JsonProperty @Getter @Setter private String toEmail;
+    public TemplatedMail withToEmail (String toEmail) { this.toEmail = toEmail; return this; }
+
     @NotNull @JsonProperty @Getter @Setter private Map<String, Object> parameters;
+    public TemplatedMail withParameters (Map<String, Object> parameters) { this.parameters = parameters; return this; }
+
+    public TemplatedMail withParameter (String name, Object value) {
+        if (this.parameters == null) this.parameters = new HashMap<>();
+        this.parameters.put(name, value);
+        return this;
+    }
 
     @Override
     public String toString() {

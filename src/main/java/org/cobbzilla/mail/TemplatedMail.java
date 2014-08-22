fa528d00b3aad61cd81f74bc39d25592c80881b8
jonathan <jonathan@kyuss.org>
2014-08-21 20:57:10 -0700

    use chained accessors to reduce boilerplate code

--- src/main/java/org/cobbzilla/mail/TemplatedMail.java
+++ src/main/java/org/cobbzilla/mail/TemplatedMail.java
@@ -3,6 +3,7 @@ package org.cobbzilla.mail;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.cobbzilla.util.string.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,34 +16,26 @@ import java.util.Map;
  * (c) Copyright 2013 Jonathan Cobb.
  * This code is available under the Apache License, version 2: http://www.apache.org/licenses/LICENSE-2.0.html
  */
+@Accessors(chain=true)
 public class TemplatedMail {
 
     private static final Logger LOG = LoggerFactory.getLogger(TemplatedMail.class);
 
     @NotNull @JsonProperty @Getter @Setter private String templateName;
-    public TemplatedMail withTemplateName (String templateName) { this.templateName = templateName; return this; }
-
     @JsonProperty @Getter @Setter private String locale;
-    public TemplatedMail withLocale (String locale) { this.locale = locale; return this; }
 
     @JsonProperty @Getter @Setter private String fromName;
-    public TemplatedMail withFromName (String fromName) { this.fromName = fromName; return this; }
     public boolean hasFromName () { return !StringUtil.empty(fromName); }
 
     @NotNull @JsonProperty @Getter @Setter private String fromEmail;
-    public TemplatedMail withFromEmail (String fromEmail) { this.fromEmail = fromEmail; return this; }
     public boolean hasFromEmail () { return !StringUtil.empty(fromEmail); }
 
     @JsonProperty @Getter @Setter private String toName;
-    public TemplatedMail withToName (String toName) { this.toName = toName; return this; }
-
     @NotNull @JsonProperty @Getter @Setter private String toEmail;
-    public TemplatedMail withToEmail (String toEmail) { this.toEmail = toEmail; return this; }
 
     @NotNull @JsonProperty @Getter @Setter private Map<String, Object> parameters;
-    public TemplatedMail withParameters (Map<String, Object> parameters) { this.parameters = parameters; return this; }
 
-    public TemplatedMail withParameter (String name, Object value) {
+    public TemplatedMail setParameter (String name, Object value) {
         if (this.parameters == null) this.parameters = new HashMap<>();
         this.parameters.put(name, value);
         return this;
